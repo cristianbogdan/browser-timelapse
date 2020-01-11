31f22fc9c7896124b3f99c7efdf527be73a8dc2a
Cristi <cristi@kth.se>
2020-01-11 20:00:15 +0200

    manage multiple timelapses at the same time

--- bin/snapshot.sh
+++ bin/snapshot.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-DIR=/data
-
 USER_DATE=$(env TZ=Europe/Bucharest date "+%d.%m_%H:%M")
-DATE=$(env TZ=Europe/Bucharest date "+%Y_%V_%m%d_%H:%M")
-YEAR_WEEK=$(env TZ=Europe/Bucharest date "+%Y_%V")
-LAST_WEEK=$(env TZ=Europe/Bucharest date --date='1 weeks ago' '+%Y_%V')
+DATE=$(env TZ=Europe/Bucharest date "+%G_%V_%m%d_%H:%M")
+YEAR_WEEK=$(env TZ=Europe/Bucharest date "+%G_%V")
+LAST_WEEK=$(env TZ=Europe/Bucharest date --date='1 weeks ago' '+%G_%V')
 
-google-chrome --no-sandbox --headless --screenshot=$DIR/$DATE.png --window-size=1280,720 https://www.google.com/maps/@44.6170083,26.4951149,12z/data=!5m1!1e1 
-convert $DIR/$DATE.png /logo_api_portrait.png -gravity northeast -geometry +10+10 -composite  -gravity North -pointsize 40 -annotate  +0+100 "${USER_DATE/_/ }" $DIR/$DATE.png
+DIR=/data/timelapses/snapshots
+EXT=jpg
+LOGO=/home/cristi/gmaps_timelapse/img/logo_api_portrait.png
 
+while read p; do
+    array=($p)
+    NAME=${array[0]}
+    RES=${array[1]}
+    URL=${array[2]}
+    if [ -n "$NAME" ] && [[ ${NAME:0:1} != "#" ]]; then
+	MYDIR=$DIR/${NAME}
+	mkdir $MYDIR &>>/dev/null
+     
+	if [ ! -f $MYDIR/.worker ]; then
+	    touch $MYDIR/.worker 
+	    FILE=$MYDIR/${NAME}_$DATE.$EXT
+	    google-chrome --no-sandbox --headless --screenshot=$FILE --window-size=$RES $URL &>>/dev/null
+	    convert  $FILE $LOGO -gravity northeast -geometry +10+10 -composite  -gravity North -pointsize 40 -annotate  +0+100 "${USER_DATE/_/ }" $FILE
+	    chmod 644 $FILE
+	    rm $MYDIR/.worker
+	else
+	    echo "skipping ${NAME}_$DATE.$EXT" >>$DIR/log.txt
+	fi
+    fi
+done </home/cristi/timelapse.conf
--- bin/video.sh
+++ bin/video.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
+DATE=$(env TZ=Europe/Bucharest date "+%G_%V_%m%d_%H:%M")
+YEAR_WEEK=$(env TZ=Europe/Bucharest date "+%G_%V")
+LAST_WEEK=$(env TZ=Europe/Bucharest date --date='1 weeks ago' '+%G_%V')
 
-DIR=/data
-USER_DATE=$(env TZ=Europe/Bucharest date "+%d.%m_%H:%M")
-DATE=$(env TZ=Europe/Bucharest date "+%Y_%V_%m%d_%H:%M")
-YEAR_WEEK=$(env TZ=Europe/Bucharest date "+%Y_%V")
-LAST_WEEK=$(env TZ=Europe/Bucharest date --date='1 weeks ago' '+%Y_%V')
+IMG_DIR=/data/timelapses/snapshots
+IMG_EXT=jpg
+DIR=/data/timelapses/videos
+EXT=mp4
 
 # wait for the last image to be made
 sleep 55
 
-mv $DIR/$YEAR_WEEK*.png $DIR/work/
+while read zeline; do
+    array=($zeline)
+    NAME=${array[0]}
+ 
+    if [ -n "$NAME" ] && [[ ${NAME:0:1} != "#" ]]; then
+	MYDIR=$DIR/$NAME
+	WORKDIR=$MYDIR/.work
+	FILE=$YEAR_WEEK.$EXT
+	
+	mkdir $MYDIR &>>/dev/null
+	mkdir $WORKDIR &>>/dev/null
+	ln -sfn $IMG_DIR/$NAME $MYDIR/snapshots 
+	
+	
+        #move images to the work dir
+	mv $IMG_DIR/$NAME/*$YEAR_WEEK*.$IMG_EXT      $WORKDIR
+	
+	# make video from the latest images
+	nice -40 ffmpeg -y -pattern_type glob -i "$WORKDIR/*.$IMG_EXT"  $WORKDIR/_$FILE </dev/null
+	# remove images
+	rm  $WORKDIR/*$YEAR_WEEK*.$IMG_EXT
 
-ffmpeg -y -pattern_type glob -i "$DIR/work/$YEAR_WEEK*.png"  $DIR/work/_$YEAR_WEEK.mp4
-
-if [ ! -f $DIR/out/$YEAR_WEEK.mp4 ]; then
-   mv $DIR/work/_$YEAR_WEEK.mp4 $DIR/out/$YEAR_WEEK.mp4
-   rm $DIR/work/$YEAR_WEEK.txt 	
-   echo "file $DIR/out/$YEAR_WEEK.mp4" > $DIR/work/$YEAR_WEEK.txt
-   echo "file $DIR/work/_$YEAR_WEEK.mp4" >> $DIR/work/$YEAR_WEEK.txt
-else
-   ffmpeg -safe 0 -f concat -y -i $DIR/work/$YEAR_WEEK.txt -c copy $DIR/out/_$YEAR_WEEK.mp4
-   mv $DIR/out/_$YEAR_WEEK.mp4 $DIR/out/$YEAR_WEEK.mp4
-fi
-rm  $DIR/work/$YEAR_WEEK*.png
-chmod 644 $DIR/out/*.mp4
+	# if we have no video
+	if [ ! -f $MYDIR/$FILE ]; then
+	    # just copy to the output
+	    mv $WORKDIR/_$FILE $MYDIR/$FILE
+	else
+	    #else concatenate them to _name
+	    ffmpeg -y -safe 0 -f concat -i <(echo "file $MYDIR/$FILE"; echo "file $WORKDIR/_$FILE")  -c copy $MYDIR/_$FILE </dev/null
+	    # remove the one we just made from images
+	    rm $WORKDIR/_$FILE
+	    # rename _name to name (overwrite the old)
+	    mv $MYDIR/_$FILE    $MYDIR/$FILE
+	fi
+	chmod 644 $MYDIR/$FILE.$EXT
+    fi
+done</home/cristi/timelapse.conf
 
